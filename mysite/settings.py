c75f72983291d189a3e2bab1d473df5a6280d8d2
Tausei722 <kusaki.kei825@gmail.com>
2024-10-14 17:29:54 +0900

    staric

--- mysite/settings.py
+++ mysite/settings.py
@@ -148,9 +148,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    STATIC_URL,
-]
+STATICFILES_DIRS = []
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
