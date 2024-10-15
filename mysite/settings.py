8bcb937cc14581876612f781fcd0e5419a1c7aa0
Tausei722 <kusaki.kei825@gmail.com>
2024-10-16 04:36:16 +0900

    dfrgetrhyjuykiulokyjthrt

--- mysite/settings.py
+++ mysite/settings.py
@@ -147,7 +147,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_ROOT = '/mysite/static'
 STATICFILES_DIRS = []
 # Default primary key field type
