7126bf942624b63236ed42c910600a41351fb52c
Tausei722 <kusaki.kei825@gmail.com>
2024-10-16 19:02:44 +0900

    staticrootfile

--- mysite/settings.py
+++ mysite/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
 STATIC_ROOT = '/static'
 STATICFILES_DIRS = [
     STATIC_ROOT,
