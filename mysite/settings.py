2d870f06dc916fc0fab858cd086fbf4a49b22e1b
Tausei722 <kusaki.kei825@gmail.com>
2024-10-16 22:33:04 +0900

    cloudinary sharp

--- mysite/settings.py
+++ mysite/settings.py
@@ -164,7 +164,7 @@ STATICFILES_DIRS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'media/'
