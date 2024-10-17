10d4995733973eb7bf76b14459e1742113167e2d
Tausei722 <kusaki.kei825@gmail.com>
2024-10-18 07:54:14 +0900

    frgoep

--- mysite/settings.py
+++ mysite/settings.py
@@ -159,10 +159,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'mysite/static/'
+STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
+STATIC_ROOT = '/static'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
