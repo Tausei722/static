244adb65b3189a568a880e836e5ca646916bee5e
Tausei722 <kusaki.kei825@gmail.com>
2024-10-18 15:59:47 +0900

    static url

--- mysite/settings.py
+++ mysite/settings.py
@@ -163,10 +163,9 @@ STATIC_URL = 'static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATIC_ROOT = '/static'
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-#     STATIC_ROOT
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -23,4 +23,4 @@ from .settings import STATIC_ROOT,STATIC_URL,MEDIA_ROOT,MEDIA_URL
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('flash/', include('flash.urls')),
-] + static(STATIC_URL, document_root=STATIC_ROOT) + static(MEDIA_URL, document_root=MEDIA_ROOT)
+] + static(MEDIA_URL, document_root=MEDIA_ROOT) + static(STATIC_URL, document_root=STATIC_ROOT)
