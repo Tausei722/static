34ae420dd182d41742189afb7cba739cf4357e77
Tausei722 <kusaki.kei825@gmail.com>
2024-10-18 21:03:39 +0900

    kkesdsdas

--- mysite/settings.py
+++ mysite/settings.py
@@ -159,13 +159,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATIC_ROOT = '/static'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static/css',
-    BASE_DIR / 'static/js',
+    BASE_DIR / 'static',
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
