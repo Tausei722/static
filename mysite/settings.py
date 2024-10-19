98b6b3f887a7b0139a03c3832ce331832c3695be
Tausei722 <kusaki.kei825@gmail.com>
2024-10-19 18:34:11 +0900

    rootnashi

--- mysite/settings.py
+++ mysite/settings.py
@@ -166,7 +166,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'flash')
+    os.path.join(BASE_DIR, 'flash/static')
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
