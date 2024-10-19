6683276f727b26c48a06341c2ec02c8f8cec3cf4
Tausei722 <kusaki.kei825@gmail.com>
2024-10-19 14:54:45 +0900

    D

--- mysite/settings.py
+++ mysite/settings.py
@@ -163,8 +163,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-    # '/mysite/static',
+    # BASE_DIR / 'static',
+    '/mysite/static',
     # os.path.join(BASE_DIR, 'static'),
 ]
 # Default primary key field type
