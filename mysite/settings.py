d4d4cc42f4c1e7f869edd8cf13232ab92d2b2bf6
Tausei722 <kusaki.kei825@gmail.com>
2024-10-19 18:21:04 +0900

    rootnashi

--- mysite/settings.py
+++ mysite/settings.py
@@ -163,10 +163,10 @@ USE_TZ = True
 #STATICFILES_DIRSからSTATIC_ROOTにコピーされたファイルをSTATIC_URLでアクセスできるので
 #それを示すURLをhtmlで読み込ませればいいはずなのにSTATIC_ROOTにコピーされていない
 #STATIC_ROOTにSTATICFILES_DIRSをcollectコマンドでコピーするルールを破るのはよくない
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'flash'),
+    os.path.join(BASE_DIR, 'flash')
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
